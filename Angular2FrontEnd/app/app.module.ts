ffd6f631a92e4aac38ed8a2fa7d966830106434f
Herczku Misi <kmisku@gmail.com>
2016-08-15 16:23:26 +0200

    Add basic angular 2 front end

---
+++ Angular2FrontEnd/app/app.module.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by herczkumihalybalazs on 2016.08.15..
+ */
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent }  from './app.component';
+
+@NgModule({
+    imports:      [ BrowserModule ],
+    declarations: [ AppComponent ],
+    bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
---
+++ Angular2FrontEnd/app/main.ts
@@ -0,0 +1,6 @@
+/**
+ * Created by herczkumihalybalazs on 2016.08.15..
+ */
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+platformBrowserDynamic().bootstrapModule(AppModule);
